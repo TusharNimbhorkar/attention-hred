172d14f87db39e6d4d138d9ff46d3257d215a72b
Daniela Solis <daniela.solis@janium.com>
2017-11-18 16:11:08 +0100

    embedding layer

--- src/Model/layers.py
+++ src/Model/layers.py
@@ -11,3 +11,18 @@ File to implement the methods that describe each layer needed for the model.
 import tensorflow as tf
 import numpy as np
 
+
+def get_embedding_layer(vocabulary_size, embedding_dims, data):
+    """
+
+    :param vocabulary_size: int for the vocabulary size
+    :param embedding_dims: int for the embedding space dimensions
+    :param data: tensor with the data to obtain the embeddings from
+    :return: embeddings for data
+
+    """
+    with tf.variable_scope('embedding_layer'):
+        embeddings_weights= tf.get_variable(name='embeddings', shape=[vocabulary_size, embedding_dims] ,
+                                            initializer= tf.truncated_normal())
+        word_embeddings= tf.nn.embedding_lookup(embeddings_weights, data)
+        return word_embeddings
