fc0926cbe985876264e0b0f8bf303b6978cdc7c9
tushar1408 <tushar.1408@gmail.com>
2017-11-18 22:09:03 +0100

    train model class

--- src/Model/train_model.py
+++ src/Model/train_model.py
@@ -5,3 +5,22 @@ IR2 - Reproduction of "A Hierarchical Recurrent
 Group 8
 
 '''
+
+import tensorflow as tf
+import numpy as np
+
+BATCH_SIZE = 50
+
+
+class Train(object):
+
+    def __init__(self):
+        some_variables = 0
+        # ...
+        #
+    def train_model(self,batch_size=None):
+        return 
+if __name__ == '__main__':
+    with tf.Graph().as_default():
+        trainer = Train()
+        trainer.train_model(batch_size=BATCH_SIZE)
