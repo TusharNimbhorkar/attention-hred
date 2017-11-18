919504a0e70b37afc899fb204689aeb270a1f93a
Daniela Solis <daniela.solis@janium.com>
2017-11-18 15:40:57 +0100

    model structure

--- src/Model/layers.py
+++ src/Model/layers.py
@@ -1,7 +1,13 @@
-'''
+"""
 IR2 - Reproduction of "A Hierarchical Recurrent
       Encoder-Decoder for Generative Context-Aware Query Suggestion"
       by Sordoni et al.
 Group 8
 
-'''
+File to implement the methods that describe each layer needed for the model.
+
+"""
+
+import tensorflow as tf
+import numpy as np
+
---
+++ src/Model/model.py
@@ -0,0 +1,24 @@
+'''
+IR2 - Reproduction of "A Hierarchical Recurrent
+      Encoder-Decoder for Generative Context-Aware Query Suggestion"
+      by Sordoni et al.
+Group 8
+
+'''
+import tensorflow as tf
+import numpy as np
+
+class HERED():
+    """"
+    This class will include the methods to build de graph to build the Recurrent
+      Encoder-Decoder.
+    """
+
+    def __init__(self):
+
+    def inference(self):
+
+    def train_step(self):
+    def get_loss(self):
+
+
