e5f900c24a324d339ec24a0a4f8e5fe85483f2d5
tushar1408 <tushar.1408@gmail.com>
2017-11-19 18:26:48 +0100

    add getbatch, initial train_model #5

--- src/Model/train_model.py
+++ src/Model/train_model.py
@@ -11,9 +11,11 @@ import numpy as np
 import _pickle as cPickle
 import sys
 import argparse
+
 # Path to get batch iterator
 sys.path.insert(0, '../sordoni/')
 import data_iterator
+from model import HERED
 
 # todo: put this stuff in arg.parse as well
 BATCH_SIZE = 50
@@ -30,10 +32,9 @@ VOCAB_FILE = '../../data/input_model/train.dict.pkl'
 TRAIN_FILE = '../../data/input_model/train.ses.pkl'
 VALID_FILE = '../../data/input_model/valid.ses.pkl'
 
+
 class Train(object):
-
     def __init__(self):
-
         self.vocab = cPickle.load(open(VOCAB_FILE, 'rb'))
         self.vocab_lookup_dict = {k: v for v, k, count in self.vocab}
 
@@ -47,23 +48,69 @@ class Train(object):
             'valid_session': VALID_FILE
         })
 
+        self.train_data.start()
+        self.valid_data.start()
+        self.vocab_size = len(self.vocab_lookup_dict)
+        # class object
+        # todo: put variables as needed and place holders
+        self.HERED = HERED()
+        self.X = tf.placeholder(tf.int64, shape=(None, None))
+        self.Y = tf.placeholder(tf.int64, shape=(None, None))
+
+        # init = tf.global_variables_initializer()
+        # summaries = tf.summary.merge_all()
+        # sess = tf.Session()
+        # sess.run(init)
+
         some_variables = 0
         # ...
         #
-    def train_model(self,batch_size=None):
-        return
-if __name__ == '__main__':
 
+    def train_model(self, batch_size=None):
+
+        init = tf.global_variables_initializer()
+
+        with tf.Session() as sess:
+
+            sess.run(init)
+
+            total_loss = 0.0
+
+            for iteration in range(MAX_STEPS):
+
+                x_batch, y_batch, seq_len = self.get_batch(train_data=self.train_data)
+
+
+
+
+
+
+        return
+
+    def get_batch(self, train_data):
+
+        data = train_data.next()
+        seq_len = data['max_length']
+        prepend = np.ones((1, data['x'].shape[1]))
+        x_data_full = np.concatenate((prepend, data['x']))
+        x_batch = x_data_full[:seq_len]
+        y_batch = x_data_full[1:seq_len + 1]
+
+
+        return x_batch, y_batch, seq_len
+
+
+if __name__ == '__main__':
     # Command line arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('--batch_size', type = int, default = BATCH_SIZE,
-                          help='Batch size to run trainer.')
-    parser.add_argument('--max_length', type = int, default = MAX_LENGTH,
-                          help='Max length.')
-    parser.add_argument('--buckets', type = int, default = N_BUCKETS,
-                          help='Number of buckets.')
-    parser.add_argument('--max_steps', type = int, default = MAX_STEPS,
-                          help='Number of steps to run trainer.')
+    parser.add_argument('--batch_size', type=int, default=BATCH_SIZE,
+                        help='Batch size to run trainer.')
+    parser.add_argument('--max_length', type=int, default=MAX_LENGTH,
+                        help='Max length.')
+    parser.add_argument('--buckets', type=int, default=N_BUCKETS,
+                        help='Number of buckets.')
+    parser.add_argument('--max_steps', type=int, default=MAX_STEPS,
+                        help='Number of steps to run trainer.')
     FLAGS, unparsed = parser.parse_known_args()
 
     with tf.Graph().as_default():
