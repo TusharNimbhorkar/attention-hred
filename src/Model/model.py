c977be56dde230dc1aa721101384b98b08fd93b1
tushar1408 <tushar.1408@gmail.com>
2017-11-20 12:38:06 +0100

    get embedding layer in model.py #4

--- src/Model/model.py
+++ src/Model/model.py
@@ -7,6 +7,7 @@ Group 8
 '''
 import tensorflow as tf
 import numpy as np
+import layers
 
 class HERED():
     """"
@@ -16,7 +17,7 @@ class HERED():
 
     def __init__(self,vocab_size=50004,embedding_dim=300, query_dim=1000, session_dim=1500,
                  decoder_dim=1000, output_dim=50004, unk_symbol=0, eoq_symbol=1, eos_symbol=2):
-        
+
         self.vocab_size = vocab_size
         self.embedding_dim = embedding_dim
         self.query_dim = query_dim
@@ -28,13 +29,23 @@ class HERED():
         self.eos_symbol = eos_symbol
 
         # create objects for query encoder, session encoder and decoder.
-        raise NotImplementedError
+        # raise NotImplementedError
 
-    def inference(self):
+    def inference(self,X):
 
         # call here tf.scan for each.
-
         # see if we should add an additional output layer after decoder.
+
+        num_of_steps = tf.shape(X)[0]
+        batch_size = tf.shape(X)[1]
+
+        embedder = layers.get_embedding_layer(vocabulary_size = self.vocab_size,
+                                              embedding_dims = self.embedding_dim, data = X)
+
+
+
+
+
 
         raise NotImplementedError
 
