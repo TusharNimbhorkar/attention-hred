3a7e20cac96c33a3e2da86fb21c8012734a64f31
tushar1408 <tushar.1408@gmail.com>
2017-11-20 12:37:23 +0100

    remove the syntax error #3

--- src/Model/layers.py
+++ src/Model/layers.py
@@ -22,10 +22,16 @@ def get_embedding_layer(vocabulary_size, embedding_dims, data):
 
     """
     with tf.variable_scope('embedding_layer'):
+
         embeddings_weights= tf.get_variable(name='embeddings', shape=[vocabulary_size, embedding_dims] ,
                                             initializer= tf.truncated_normal())
+
         word_embeddings= tf.nn.embedding_lookup(embeddings_weights, data)
+
         return word_embeddings
 def get_query_encoder_layer():
+    return
 def get_session_enconder_layer():
+    return
 def get_decoder_layer():
+    return
