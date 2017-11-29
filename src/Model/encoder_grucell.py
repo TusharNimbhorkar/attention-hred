c34e74bf2528a4fe49eb5a9009ae3a40bfcbb4f2
mrivar <martin7557@gmail.com>
2017-11-29 12:58:25 +0100

    encoder_grucell.py: improve encoder grucell

--- src/Model/encoder_grucell.py
+++ src/Model/encoder_grucell.py
@@ -33,16 +33,39 @@ class Encoder(object):
         self.gru_cell = tf.contrib.rnn.GRUCell(num_hidden, kernel_initializer=initializer_weights, bias_initializer=initializer_biases)
         #self.zero_state = self.gru_cell.zero_state(batch_size, tf.float32)
 
-    def compute_state(self, x, batch_size, query_or_session_length, state=None):
+    def length(sequence):
         """
-        :x: array of embeddings of query batch and reset vector 1 when embedding is not end of session/query 0 when it is
-        :return: query representation tensor
+         :sequence: batch of padded length; with zero vectors after eoq_symbol 
+         :return:   vector determining length of queries (at what point the eoq_symbol is encountered)
+        """
+        used = tf.sign(tf.reduce_max(tf.abs(sequence), 2))
+        length = tf.reduce_sum(used, 1)
+        length = tf.cast(length, tf.int32)
+        return length
+
+    def compute_state(self, x, batch_size, state=None):
+        """
+        :x:      query/session batch of padded length [batch_size x max_length x out_size]
+        :return: state representation per step of size [batch_size x max_length x out_size]
         """
         # Initialise recurrent state
         if not state:
             state = self.gru_cell.zero_state(batch_size, tf.float32)
+
         # Calculate RNN states
-        for step in range(query_or_session_length):
-            _, state = self.gru_cell(x[step], state)
+        _, state = tf.nn.dynamic_rnn(
+            self.gru_cell,
+            x,
+            dtype=tf.float32,
+            sequence_length=self.length(x),
+            initial_state=state)
 
         return state
+
+    def get_final_state(self, x, states):
+        """
+        :x:      query/session batch of padded length [batch_size x max_length x out_size]
+        :states: output of compute_state
+        :return: vector of all the final states per query
+        """
+        return states[:, self.length(x)-1]
