eceb76a49589a879592e2c6818e3d1d807e271dd
mrivar <martin7557@gmail.com>
2017-11-29 14:31:02 +0100

    decoder_grucell: improved compute_prediction

--- src/Model/decoder_grucell.py
+++ src/Model/decoder_grucell.py
@@ -64,26 +64,35 @@ class Decoder(object):
 
         return outputs, states
 
-    def compute_prediction(self, session_state, eoq_symbol):
-        # is output of decoder a vector of size=vocab_size (all 0s except the value corresponding to the word),
-        #or an embedding (and then we look for the word with the closest embedding)?
-        state = tf.tanh(tf.sum(tf.matmul(session_state, self.Do), self.Bo))
+    def compute_prediction(self, session_state, query_encoder_last_state, sequence_length):
+        """
+        :session_state:            state to initialize the recurrent state of the decoder
+        :query_encoder_last_state: last encoder state of the previous query to be used as first input
+        """
+        outputs       = []
+        state         = tf.tanh(tf.sum(tf.matmul(session_state, self.Do), self.Bo))
+        output, state = self.gru_cell(query_encoder_last_state, state)
+        output        = self.prediction(output)
+        outputs.append(output)
+
         # Calculate RNN states
-        outputs = []
-        output  = self.gru_cell(state)
-        output  = self.prediction(state)
-        stop_after_ten_words = 10
-        
-        while calculate_word(output) != eoq_symbol and stop_after_ten_words>0:
+        # TODO: how do I check that the output is the eoq_symbol!! (or do I only run seq_length and we will see afterwards)
+        stop_after = sequence_length
+        while stop_after>0:
             output, state = self.gru_cell(output, state)
             output        = self.prediction(output)
             outputs.append(output)
-            stop_after_ten_words -= 1
+            stop_after -= 1
+        return outputs
+
+    def check_if_eoq(self, gru_output):
+        # Check if the output is the end of query symbol
+        raise NotImplementedError
 
     def calculate_word(self, gru_output):
         # Calculate word given the output of the Decoder 
-        raise BaseException('Not implemented')
+        raise NotImplementedError
 
     def prediction(self, gru_output):
         # Transform output to prediction given the output of the Decoder
-        raise BaseException('Not implemented')
+        raise NotImplementedError
