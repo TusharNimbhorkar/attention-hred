28ccf41107c2e81a01f1a3fe458b7b055f395ccc
tushar1408 <tushar.1408@gmail.com>
2017-12-11 14:56:23 +0100

    new get batch for train

---
+++ src/Model/get_batch.py
@@ -0,0 +1,37 @@
+import _pickle as cPickle
+import numpy as np
+train_file = 'allq_train.p'
+valid_file = 'allq_valid.p'
+
+# random_list = range(0,1000,50)
+# element = 0
+
+def get_batch( random_list,type='train',element=50, batch_size=50,max_len=50):
+    if type == 'train':
+        data = cPickle.load(open(train_file, 'rb'))
+        print('getBatch',len(data))
+        random_list.remove(element)
+        train_list = []
+        for i in range(element,element+batch_size+1):
+            train_list.append(data[i])
+        batch_max_len = 0
+        for i in train_list:
+            curlen =len(i)
+            if curlen>batch_max_len:
+                batch_max_len=curlen
+        if batch_max_len>max_len:
+            batch_max_len=max_len
+
+        padded_train = []
+        for i in train_list:
+            listofzeros = [0] * batch_max_len
+            listofzeros[:len(i)] = i
+            padded_train.append(listofzeros)
+        full_batch = np.asarray(padded_train)
+        y_batch=full_batch[1:]
+        x_batch=full_batch[:-1]
+        return x_batch,y_batch,batch_max_len,random_list
+
+
+
+
