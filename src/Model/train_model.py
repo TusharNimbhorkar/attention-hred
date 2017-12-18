f1df4009db60b342969ad7706ff06c4e874fb673
mrivar <martin7557@gmail.com>
2017-12-18 17:34:23 +0100

    restore old method just in case

--- src/Model/train_model.py
+++ src/Model/train_model.py
@@ -39,8 +39,8 @@ SESSION_DIM = 1500
 VOCAB_FILE = '../../data/input_model/train.dict.pkl'
 TRAIN_FILE = '../../data/input_model/train.ses.pkl'
 VALID_FILE = '../../data/input_model/valid.ses.pkl'
-train_file = '../../data/new_new_batch/allq_train.p'
-valid_file = '../../data/new_new_batch/allq_train.p'
+train_file = '../../data/new_batch/allq_train.p'
+valid_file = '../../data/new_batch/allq_train.p'
 
 
 class Train(object):
@@ -119,8 +119,8 @@ class Train(object):
     def train_model(self, batch_size=None, restore = False):
 
         # batch parameters,train
-        #train_list = list(range(0, len(self.train_data)-150, batch_size))
-        train_list = list(range(0, len(self.train_data)))
+        train_list = list(range(0, len(self.train_data)-150, batch_size))
+        #train_list = list(range(0, len(self.train_data)))
 
 
         # summaries = tf.summary.merge_all()
