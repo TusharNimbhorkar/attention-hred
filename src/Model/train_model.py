baa58bbebeb45273d6ba4abda9fce6e38ddaa508
Daniela Solis <daniela.solis@janium.com>
2017-12-18 10:35:48 +0100

    loss mask fixed , attention bool included #5

--- src/Model/train_model.py
+++ src/Model/train_model.py
@@ -194,7 +194,7 @@ class Train(object):
                                                                       batch_size=self.config.batch_size,
                                                                       max_len=self.config.max_length, eoq=self.HERED.eoq_symbol)
 
-                    predictions = sess.run([self.HERED.validation(X = self.X, Y= self.Y)], feed_dict={self.X: x_batch, self.Y: y_batch})
+                    predictions = sess.run([self.HERED.validation(X = self.X, Y= self.Y, attention=False)], feed_dict={self.X: x_batch, self.Y: y_batch})
                     accuracy = self.get_accuracy(predictions , y_batch)
 
                     # print(self.get_length(y_batch))
