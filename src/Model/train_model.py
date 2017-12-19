752695f02c4808911e4339d2d956ebe85eee76d8
mrivar <martin7557@gmail.com>
2017-12-19 23:39:53 +0100

    reduce learning rate

--- src/Model/train_model.py
+++ src/Model/train_model.py
@@ -178,7 +178,7 @@ class Train(object):
                                                              batch_size=self.config.batch_size,
                                                              max_len=self.config.max_length, eoq=self.HERED.eoq_symbol)
 
-            self.config.max_steps = 47500
+            self.config.max_steps = 40000
             for iteration in range(global_step, self.config.max_steps):
 
                 #todo:
@@ -404,7 +404,7 @@ if __name__ == '__main__':
 
     # Training params
     parser.add_argument('--batch_size', type=int, default=BATCH_SIZE, help='Batch size to run trainer.')
-    parser.add_argument('--learning_rate', type=float, default=2e-3, help='Learning rate')
+    parser.add_argument('--learning_rate', type=float, default=2e-4, help='Learning rate')
     parser.add_argument('--learning_rate_decay', type=float, default=0.96, help='Learning rate decay fraction')
     parser.add_argument('--max_steps', type=int, default=MAX_STEPS, help='Number of steps to run trainer.')
 
