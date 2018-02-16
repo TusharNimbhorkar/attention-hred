4707a37b0cde25419f618d597280de4a1827f3f2
Daniela Solis <daniela.solis@janium.com>
2018-02-16 16:28:38 +0100

    Cleaning debug comments #3

--- src/Model/layers.py
+++ src/Model/layers.py
@@ -51,14 +51,7 @@ def output_layer(embedding_dims, num_hidden, vocabulary_size, state, word):
                                    initializer=tf.truncated_normal_initializer(mean=0.0, stddev=1.0))
 
         y_one_hot = tf.one_hot(word, depth=vocabulary_size)
-        start_word = tf.expand_dims(tf.zeros([tf.shape(word)[0], vocabulary_size]),1)
-        # y_one_hot_shifted = tf.concat([start_word,y_one_hot],1)
-        #y_embedding_onehot = get_embedding_layer(vocabulary_size=vocabulary_size,
-        #                                         embedding_dims=embedding_dims, scope='output_embedding', data=y_one_hot_shifted)
 
-        #return tf.matmul(state, tf.transpose(H_ouput)) + tf.transpose(tf.cast(y_embedding_onehot, tf.float32)) + b_output
-        # tf.einsum('bsh,eh->bse',state, H_ouput)
-        # tf.einsum('bsv,ev->bse',y_one_hot, E_output)
         return tf.einsum('bsh,eh->bse', state, H_ouput) + tf.einsum('bsv,ev->bse',y_one_hot, E_output) + b_output
 
 
@@ -94,29 +87,14 @@ def get_context_attention(annotations, decoder_states, decoder_dims, encoder_dim
         w = tf.get_variable(name='weight_context', shape=(2 * encoder_dims, decoder_dims),
                         initializer=tf.random_normal_initializer(stddev=0.01))
     # Calculate alphas for the context vector
-    #a = tf.einsum('ed, bmd -> ebm', w, decoder_states)
-    #a = tf.einsum('bmd, ed --> ebm', decoder_states, w)
     w_tile = tf.tile(tf.expand_dims(w, 0), (batch_size, 1, 1))
     dec_w = tf.matmul(decoder_states, tf.transpose(w_tile, perm=[0, 2, 1]))  # batch x max_steps x 2 x enc_dims
-    #print(dec_w)
     annotations = tf.transpose(annotations, perm=[1, 0, 2])
-    #print(annotations)
     annotations_mul = tf.matmul(annotations, tf.transpose(dec_w, perm=[0, 2, 1]))
-    #print(annotations_mul)
-    #b = tf.einsum('bme, ebm -> bm', annotations, dec_w)
-    alphas = tf.nn.softmax(annotations_mul)  # batch_size x seq_leght
-    #print(alphas)
+    alphas = tf.nn.softmax(annotations_mul)  # batch_size x seq_length
     # Calculate context vector
     weighted_annotations = tf.matmul(alphas, annotations)
-    #print(weighted_annotations)
-    # alphas_tile = tf.tile(tf.expand_dims(alphas,3), (1, 1, 1, 2 * encoder_dims))
-    # annotations_tile = tf.tile(tf.expand_dims(annotations, 2), (1, 1, max_length, 1))
-    # weighted_annotations = tf.matmul(tf.transpose(alphas_tile, perm=[0, 1, 3, 2]), annotations_tile)
-    #tf.einsum('bm, bme -> bme', alphas, annotations)
-    #print(weighted_annotations)
-    # weighted_annotations is (b x m x 2e x 2e) and we need context to be b x m so we reduce sum twice TODO: ask if this is alright
     context = tf.reduce_sum(weighted_annotations, axis=2)  # reduce over 2 x enc_dims twice
-    #print(context)
     return context
 
 
@@ -150,8 +128,6 @@ def bidirectional_layer(x, encoder_dims, batch_size):
         initial_state=gru_cell_bi.zero_state([batch_size], tf.float32),
         scope = 'bidirectional')
 
-    #print(states_forward)
-
     # Backward pass - returns a list of size max_steps of tensors of shape [batch_size, hidden_dims]
     states_backward, _ = tf.nn.static_rnn(
         gru_cell_bi,
@@ -161,7 +137,6 @@ def bidirectional_layer(x, encoder_dims, batch_size):
         initial_state=gru_cell_bi.zero_state([batch_size], tf.float32),
         scope = 'bidirectional')
 
-    #print(states_backward)
 
     return tf.concat([states_forward, states_backward], axis=2)
 
